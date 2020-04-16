87bdd0f7c23f40e879d686352f41d04f16f8adf6
William <xwilliamy@gmail.com>
2020-04-16 13:39:54 -0400

    Modified pose_to_images to display all keypoints in preparation for selecting the correct person in multiperson setting

--- image_to_video.py
+++ image_to_video.py
@@ -1,7 +1,24 @@
 import cv2
 import numpy as np
 import glob
- 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_dir",
+                    help="Path to images",
+                    default="/Users/will.i.liam/Desktop/final_project/phoan/images/",
+                    type=str)
+
+parser.add_argument("--output_dir",
+                    help="output path",
+                    default="",
+                    type=str)
+
+parser.add_argument("--output_filename",
+                    help="output filename",
+                    default="project.mp4",
+                    type=str)
+
 img_array = []
 filenames = sorted(glob.glob('/Users/will.i.liam/Desktop/final_project/phoan/images/*.jpg'))
 
@@ -14,9 +31,9 @@ for filename in filenames:
     size = (width,height)
     img_array.append(img)
     count += 1
- 
- 
-out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+
+fourcc_format = cv2.VideoWriter_fourcc(*'MP4V')
+out = cv2.VideoWriter('project.mp4', fourcc_format, 30, size)
  
 for i in range(len(img_array)):
     out.write(img_array[i])
--- pose_to_images.py
+++ pose_to_images.py
@@ -4,28 +4,22 @@ import cv2
 import numpy as np
 import math
 from matplotlib import pyplot as plt
-from dataset.output_helper import save_batch_images
+import argparse
 
-# l ear, l eye, nose, r eye, r ear
-# l wrist, l elbow, l shoulder, middle, r shoulder, r elbow, r wrist
-# l hip, mid hip, r hip
-# l knee, r knee
-# l ankle, r ankle
+"""
+draw_pose_figure adapted from:
+https://github.com/xrenaa/Music-Dance-Video-Synthesis/blob/master/dataset/output_helper.py
+"""
 
-joint_to_limb_heatmap_relationship = [
-    [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10],
-    [1, 11], [11, 12], [12, 13], [1, 0], [0, 14], [14, 16], [0, 15], [15, 17],
-    [2, 16], [5, 17]]
+parser = argparse.ArgumentParser()
 
+# 19 and 20 are chin and top of head
 joint_to_limb_heatmap_relationship = [
     [1,8],   [1,2],   [1,5],   [2,3],   [3,4],
     [5,6],   [6,7],   [8,9],   [9,10],  [10,11],
     [8,12],  [12,13], [13,14], [1,0],   [0,15],
     [15,17], [0,16],  [16,18]]
 
-# 19 and 20 are chin and top of head
-
-#for plot usage
 colors = [
     [255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0],
     [85, 255, 0], [0, 255, 0], [0, 255, 85], [0, 255, 170], [0, 255, 255],
@@ -33,21 +27,27 @@ colors = [
     [255, 0, 255], [255, 0, 170], [255, 0, 85], [255, 0, 0], [255,0,0],
     [255,0,0], [255,0,0], [255,0,0], [255,0,0]]
 
+def imshow(image):
+    plt.imshow(image[:,:,[2,1,0]].astype(np.uint8))
+    plt.show()
 
-def draw_pose_figure(coors,height=1500,width=1500,limb_thickness=4):
-    canvas = np.ones([height,width,3])*255
-    canvas = canvas.astype(np.uint8)
+def add_pose_to_canvas(person_id, coors, canvas, limb_thickness=4):
     limb_type = 0
-    for joint_relation in  joint_to_limb_heatmap_relationship:
-        '''
-        if(limb_type >= 20):
-            break
-        '''
-        joint_coords = coors[joint_relation]
+    labeled = False
+    for joint_relation in  joint_to_limb_heatmap_relationship: # [1, 8] for example
+        joint_coords = coors[joint_relation] # use it as indices 
+        if 0 in joint_coords: # if a keypoint has 0
+            # ignore keypoints that are not predicted
+            limb_type += 1
+            continue
         for joint in joint_coords:  # Draw circles at every joint
-            #print('joint',joint)
+            if not labeled:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(canvas, str(person_id),
+                            tuple(joint[0:2].astype(int)), font, 3, (0,0,0), thickness=5)
+                labeled = True
             cv2.circle(canvas, tuple(joint[0:2].astype(
-                        int)), 4, (0,0,0), thickness=-1)  
+                        int)), 4, (0,0,0), thickness=-1)
         coords_center = tuple(
                     np.round(np.mean(joint_coords, 0)).astype(int))
         limb_dir = joint_coords[0, :] - joint_coords[1, :]
@@ -59,44 +59,75 @@ def draw_pose_figure(coors,height=1500,width=1500,limb_thickness=4):
         limb_type += 1
     return canvas
 
+    
+def draw_pose_figure(person_id, coors, height=1500, width=1500, limb_thickness=4):
+    canvas = np.ones([height,width,3])*255
+    canvas = canvas.astype(np.uint8)
+    limb_type = 0
+    return add_pose_to_canvas(person_id, coors, canvas, limb_thickness)
 
-def imshow(image):
-    plt.imshow(image[:,:,[2,1,0]].astype(np.uint8))
-    plt.show()
 
-# cd content/output
-path_to_json = '/Users/will.i.liam/Desktop/final_project/openpose/output/'
-image_path = '/Users/will.i.liam/Desktop/final_project/phoan/images/'
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+parser.add_argument("--input_dir",
+                    help="Path to directory containing json keypoints",
+                    default='/Users/will.i.liam/Desktop/final_project/openpose/output/',
+                    type=str)
+parser.add_argument("--output_dir",
+                    help="Path to output directory containing images",
+                    default='/Users/will.i.liam/Desktop/final_project/phoan/images/',
+                    type=str)
+
+args = parser.parse_args()
+
+json_files = [pos_json for pos_json in os.listdir(args.input_dir) if pos_json.endswith('.json')]
 json_files = sorted(json_files)
 
-prev_keypoints = [0] * 50
 first_file = True
 shift = np.array([0, 0])
 
 for json_file in json_files:
-    data = json.load(open(path_to_json + json_file))
+    data = json.load(open(args.input_dir + json_file))
 
     # shift everything else
+    '''
     keypoints = []
     count = 0
-    index = 0
+    '''
     if len(data['people']) < 1:
+        # keypoints = [0] * 42
         continue
-    for keypoint in data['people'][0]['pose_keypoints_2d']:
-        # if count is 2, ignore (the confidence)
-        if count != 2:
-            keypoints.append(keypoint)
-            '''
-            if keypoint == 0:
-                keypoints.append(prev_keypoints[index])
-            else:
-                keypoints.append(keypoint)
-            '''
-            count += 1
-            index += 1
-        else:
+    else:
+        print(len(data['people']))
+        for index, person in enumerate(data['people']):
+            keypoints = []
             count = 0
+            for keypoint in person['pose_keypoints_2d']:
+                if count != 2:
+                    keypoints.append(keypoint)
+                    count += 1
+                else:
+                    count = 0
+            # pass person['person_id'] and keypoints to canvas
+            np_keypoints = np.array(keypoints).reshape(-1, 2)
+            if index == 0:
+                # create the canvas
+                canvas = draw_pose_figure(person['person_id'],
+                                 np_keypoints)
+                index += 1
+            else:
+                # add to the canvas
+                add_pose_to_canvas(person['person_id'],
+                                   np_keypoints,
+                                   canvas)
+    '''
+    # this was saving only one pose per frame
+    else:
+        for keypoint in data['people'][0]['pose_keypoints_2d']:
+            # if count is 2, ignore (the confidence)
+            if count != 2:
+                keypoints.append(keypoint)
+                count += 1
+            else:
+                count = 0
 
 
     np_keypoints = np.array(keypoints).reshape(-1, 2)
@@ -104,12 +135,13 @@ for json_file in json_files:
         shift = np.array([1000, 1250] - np_keypoints[8])
 
     shifted = np_keypoints + shift
-
-    file_name = image_path + json_file[:-15] + ".jpg"
-    prev_keypoints = keypoints
+    '''
+    file_name = args.output_dir + json_file[-20:-15] + ".jpg"
     print(file_name)
-    imshow(draw_pose_figure(shifted))
-    # in their code, real is stacked vertically on top of fake
+
+    # imshow(canvas)
+    cv2.imwrite(file_name, canvas)
+    # imshow(draw_pose_figure(1, np_keypoints))
     # cv2.imwrite(file_name, draw_pose_figure(shifted))
 
     # imshow(draw_pose_figure(np_keypoints))
