16fd17b08edaa5bdc3bbc4da1f76538cd507eff1
William <xwilliamy@gmail.com>
2020-04-22 15:49:58 -0400

    moving data and script into github repo for easier testing

--- script.py
+++ script.py
@@ -1,18 +1,21 @@
 import torch
 from torch.utils import data
 from classes import AudioToPosesDataset
+from model import AudioToJoints
+import torch.nn as nn
 
-root_dir = '/Users/will.i.liam/Desktop/final_project/VEE5qqDPVGY/data/'
+root_dir = 'data/'
 mfcc_file = root_dir + 'VEE5qqDPVGY_153_7878_mfccs.npy'
 pose_file = root_dir + 'processed_compiled_data_line_0.npy'
 
-dataset = AudioToPosesDataset(mfcc_file, pose_file)
+seq_len = 1
+dataset = AudioToPosesDataset(mfcc_file, pose_file, seq_len)
 
-params = {'batch_size':256,
+params = {'batch_size':1,
           'shuffle':False,
-          'num_workers': 1}
+          'num_workers': 1
+          }
 generator = data.DataLoader(dataset, **params)
-
 
 for epoch in range(1):
     count = 0
