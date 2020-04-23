4b7c4f8039c51df686f639602fb92d8cd077ba90
William <xwilliamy@gmail.com>
2020-04-23 13:13:02 -0400

    Might be better to do -1 or not inside audio_to_mfcc and compile rather than here

--- data_utils/read_desired_frames.py
+++ data_utils/read_desired_frames.py
@@ -1,9 +1,17 @@
-def read_desired_frames(filename):
+import numpy as np
+
+def read_desired_frames(filename, frame_rate, target_frame_rate):
     tuples = []
     with open(filename, 'r') as f:
         temp = f.read().splitlines()
         print(temp)
         for line in temp:
-            desired = tuple(int(i) for i in line.split(','))
-            tuples.append(desired)
+            # change 1 and 2 here
+            desired = [int(i) for i in line.split(',')]
+            desired[1] = int(np.ceil(desired[1] / frame_rate) * target_frame_rate)
+            desired[2] = int(np.floor(desired[2] / frame_rate) * target_frame_rate) - 1
+            adjusted_desired = tuple(desired)
+            print("converting frames")
+            print(adjusted_desired)
+            tuples.append(adjusted_desired)
     return tuples
