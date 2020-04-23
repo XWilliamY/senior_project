dc13222775fbd371ead8989215885aad5ccdb704
William <xwilliamy@gmail.com>
2020-04-23 13:16:35 -0400

    updated script

--- script.py
+++ script.py
@@ -5,10 +5,10 @@ from model import AudioToJoints
 import torch.nn as nn
 
 root_dir = 'data/'
-mfcc_file = root_dir + 'VEE5qqDPVGY_153_7878_mfccs.npy'
+mfcc_file = root_dir + 'VEE5qqDPVGY_210_9810_mfccs.npy'
 pose_file = root_dir + 'processed_compiled_data_line_0.npy'
 
-seq_len = 4
+seq_len = 3
 dataset = AudioToPosesDataset(mfcc_file, pose_file, seq_len)
 
 print(dataset.getDataDims())
@@ -25,7 +25,7 @@ for epoch in range(1):
     count = 0
     
     for mfccs, poses in generator:
-        # print(mfccs.shape, poses.shape, count)
+        print(mfccs.shape, poses.shape, count)
         count += 1
 
         # model computations
