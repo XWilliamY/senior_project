09bc67f05e3d0625e02c0641a7a5cc3a25d1e6b8
William <xwilliamy@gmail.com>
2020-04-24 17:19:31 -0400

    This is probably abusing dataloader, but it's possible now to feed in path to audio file without accompanying poses. Useful for testing

--- classes.py
+++ classes.py
@@ -5,23 +5,35 @@ from torch.utils.data import Dataset, DataLoader
 
 class AudioToPosesDataset(Dataset):
     """ Aligns mfcc .npy file to poses .npy file """
-
-    def __init__(self, mfcc_file, pose_file, seq_len):
+    def __init__(self, mfcc_file=None, pose_file=None, seq_len=1):
         """
+        If given just an mfcc_file, configure data file for testing
+        Otherwise, creates audio_video pairs
+
         Args:
              mfcc_file (string): Path to .npy file w/ mfccs
-             pose_file (string): Path to .npy file w/ joint keypoints
+             pose_file (optional, string): Path to .npy file w/ joint keypoints
         """
         self.mfcc_file = mfcc_file
-        self.pose_file = pose_file
+        self.pose_file = pose_file 
         self.seq_len = seq_len
 
         self.mfccs = np.load(self.mfcc_file)
-        # need to transform mfccs since it's currently
-        # mfcc_features by total_frames
+        # transform to total_frames by mfcc_features
         self.mfccs = self.mfccs.T.astype('float64')
-        self.poses = np.load(self.pose_file).astype('float64')
+        print(self.mfccs.shape)
+        if self.pose_file:
+            self.poses = np.load(self.pose_file).astype('float64')
+        else: # create dummy
+            self.poses = np.zeros([int(np.floor(self.mfccs.shape[0] / 3)),
+                                   19,
+                                   2])
 
+    def hasPoses(self):
+        if self.pose_file:
+            return True
+        return False
+    
     def getDataDims(self):
         """
         MFCC dimensions
@@ -33,7 +45,7 @@ class AudioToPosesDataset(Dataset):
         return self.mfccs.shape[-1] * 3
 
     def getSingleOutputFeatureDims(self):
-        return 38
+        return self.poses.shape[1] * self.poses.shape[2]
     
     def getDimsPerBatch(self):
         return self.getSingleInputFeatureDims(), self.getSingleOutputFeatureDims()
@@ -56,7 +68,7 @@ class AudioToPosesDataset(Dataset):
 
         mfcc_idx = self.seq_len * 3 # 3 mfcc frames per timestep
         X = self.mfccs[idx * mfcc_idx : (idx + 1) * mfcc_idx, :]
-        X = X.reshape([self.seq_len, -1]) 
+        X = X.reshape([self.seq_len, -1])
         y = self.poses[idx * self.seq_len : (idx+1) * self.seq_len]
         y = y.reshape([self.seq_len, -1])
         return X, y
