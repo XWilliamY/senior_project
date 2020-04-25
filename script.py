d4ae6db016509849bfab5a2b40b53f5af47ef194
William <xwilliamy@gmail.com>
2020-04-25 03:37:48 -0400

    train validation sets

--- script.py
+++ script.py
@@ -11,17 +11,25 @@ pose_file = None
 
 seq_len = 3
 dataset = AudioToPosesDataset(mfcc_file, pose_file, seq_len)
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
 
+'''
 print(dataset.hasPoses())
 print('loaded dataset')
 print(dataset.getDataDims())
 print(dataset.getDimsPerBatch())
+'''
 
 params = {'batch_size':4,
           'shuffle':False,
           'num_workers': 1
           }
 
+print(len(dataset))
+train, validate = data.random_split(dataset, [train_size, test_size])
+print(len(train))
+print(len(validate))
 generator = data.DataLoader(dataset, **params)
 print(generator.dataset.getDimsPerBatch())
 for epoch in range(1):
