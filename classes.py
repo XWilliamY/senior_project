01b55d5d6caec9460efa46202f8cdc009f39881e
William <xwilliamy@gmail.com>
2020-04-28 02:01:28 -0400

    no print

--- classes.py
+++ classes.py
@@ -50,7 +50,6 @@ class AudioToPosesDirDataset(Dataset):
         mfcc_idx = self.seq_len * 3
         X = self.mfccs[file_idx][target_idx * mfcc_idx : (target_idx + 1) * mfcc_idx]
         X = X.reshape([self.seq_len, -1])
-        print(X.shape)
         y = target_pose.reshape([self.seq_len, -1])
 
         X = torch.from_numpy(X)
